962892eecb638fb505988e2f314f8c9290c2b8ec
mdelarocha <delarochamiguelalex@gmail.com>
2018-11-11 18:28:54 -0600

    Initial Commit
    Initial commit of first files.

---
+++ main_writer.py
@@ -0,0 +1,23 @@
+"""
+    This program is part of a Used Car Recommender System using ASP and s(ASP).
+    Developed by: Miguel de la Rocha
+    CS 4v98, Fall 2018
+"""
+
+import csv
+
+def __main__: 
+    with open('tc20171021.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        line_count = 0
+        for row in csv_reader: 
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                print(f'\t{row[0]} and {row[1]}')
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+        
+
+
