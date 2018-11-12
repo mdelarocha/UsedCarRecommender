cb42d5ffb955d534686f355a7b4a02d859958aaf
mdelarocha <delarochamiguelalex@gmail.com>
2018-11-12 15:31:15 -0600

    Some Commit
    Fixed some bugs

--- main_writer.py
+++ main_writer.py
@@ -3,10 +3,10 @@
     Developed by: Miguel de la Rocha
     CS 4v98, Fall 2018
 """
-
+ 
 import csv
 
-def __main__: 
+if __name__ == '__main__':
     with open('data/tc20171021.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
@@ -18,6 +18,3 @@ def __main__:
                 print(f'\t{row[0]} and {row[1]}')
                 line_count += 1
         print(f'Processed {line_count} lines.')
-        
-
-
