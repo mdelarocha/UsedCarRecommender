a3b2b1bda0ed5dcb618ccb4acfe341a2d89446e0
mdelarocha <delarochamiguelalex@gmail.com>
2018-11-15 18:23:57 -0600

    main_writer parses the data and writes it into CarFact.lp

--- main_writer.py
+++ main_writer.py
@@ -7,6 +7,9 @@
 import csv
 
 if __name__ == '__main__':
+    # Open a file for writing
+    f = open("CarFact.lp", "a")
+    # Open the file for the car data
     with open('data/tc20171021.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
@@ -15,6 +18,20 @@ if __name__ == '__main__':
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
-                print(f'\t{row[0]} and {row[1]}')
+                # _car(Miles, Year, Brand, Model, Price).
+                # 0 = ID, 1 = Price, 2 = Year, 3 = Mileage, 4 = City, 5 = State, 6 = VIN, 7 = Make, 8 = Model
+                brand = str(row[7])
+                model = str(row[8])
+                if "-" in brand:
+                    brand = brand.replace('-', '_')
+                if "-" in model:
+                    model = model.replace('-', '_')
+                elif "." in model:
+                    model = model.replace('.', '_')
+
+                line = "_car(" + row[3] + ", " + row[2] + ", " + brand + ", " + model + ", " + row[1] + ").\n"
+
+                f.write(line)
+                print("Writing line: ", line)
+
                 line_count += 1
-        print(f'Processed {line_count} lines.')
