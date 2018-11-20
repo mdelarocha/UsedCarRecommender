e744b01974b97b2606b7f7193ff0b6ebe9154e23
mdelarocha <delarochamiguelalex@gmail.com>
2018-11-20 11:42:06 -0700

    Modified the rules and the facts.

--- main_writer.py
+++ main_writer.py
@@ -5,41 +5,73 @@
 """
  
 import csv
+import datetime
+
+
+# Function to verify if a given string has a digit
+def hasNumbers(inputString):
+     return any(char.isdigit() for char in inputString)
+
+# Function to clean a given string
+def cleanString(inputString):
+    modifiedString = inputString
+
+    if "-" in inputString:
+        modifiedString = inputString.replace('-', '_')
+
+    if "." in inputString:
+        modifiedString = inputString.replace('.', '_')
+
+    return modifiedString.lower()
+
 
 if __name__ == '__main__':
     # Open a file for writing
-    f = open("CarFact_TEST.lp", "a")
+    f = open("CarFacts_TEST.lp", "a")
+    # Constant for year where the program is executed
+    YEAR = datetime.datetime.now().year
+
     # Open the file for the car data
     with open('data/tc20171021.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
         name = ""
+
+        # Go through each row of the document and write to a file
         for row in csv_reader:
-            # _car(Miles, Year, Brand, Model, Price).
-            # 0 = ID, 1 = Price, 2 = Year, 3 = Mileage, 4 = City, 5 = State, 6 = VIN, 7 = Make, 8 = Model
-            brand = str(row[7])
-            model = str(row[8])
-            if "-" in brand:
-                brand = brand.replace('-', '_')
-            if "-" in model:
-                model = model.replace('-', '_')
-            elif "." in model:
-                model = model.replace('.', '_')
-            
+            # Skip the header line
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
-            elif line_count > 0:
-                if name == row[7]:
-                    continue
-                else:
-                    name = row[7]
-                    line = "_car(" + row[3] + ", " + row[2] + ", " + brand + ", " + model + ", " + row[1] + ").\n"
-                    f.write(line)
-            else:
-                line = "_car(" + row[3] + ", " + row[2] + ", " + brand + ", " + model + ", " + row[1] + ").\n"
+                continue
 
-                f.write(line)
-                print("Writing line: ", line)
+            # _car(Miles, Age, Brand, Model, Price).
+            # 0 = ID, 1 = Price, 2 = Year, 3 = Mileage, 4 = City, 5 = State, 6 = VIN, 7 = Make, 8 = Model
+            # Assign each field to a variable for pre-processing
+            mileage = int(row[3])
+            age = int(row[2])
+            price = int(row[1])
+            brand = str(row[7])
+            model = str(row[8])
 
-                line_count += 1
+            # Reject any model that has a number in it
+            # TODO: Make it so that car numbers are valid
+            if hasNumbers(model) or hasNumbers(brand):
+                continue
+
+            # If not rejected, make sure to get the brand and model in lowercase
+            brand = cleanString(brand)
+            model = cleanString(model)
+
+            # Get the age of the car
+            age = YEAR - age
+
+            # Format each car as an ASP fact
+            line = "_car({0}, {1}, {2}, {3}, {4}).\n".format(mileage, age, brand, model, price)
+
+            # Write the line to file
+            f.write(line)
+            line_count += 1
+
+            print("Writing line: ", line_count)
+
